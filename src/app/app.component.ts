80b02585b75cf5a8ed70503b7f1ea5ef2d091b6e
Vasyl Korol <vasyl.korol@polcode.net>
2018-12-04 20:34:02 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'makeTesting';
+}
